cd3948e51184c142aa1b45ef814e999fc684ff8f
kaguachagmira <mantillagka@hotmail.com>
2020-06-30 16:58:27 -0500

    Simplificación de código añadido de funciones y distancias entre dos puntos

--- Leaflet JS/index.js
+++ Leaflet JS/index.js
@@ -1,59 +1,56 @@
 var map = L.map('map').setView([-0.067116, -78.403128], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([-0.257128, -78.546270]).addTo(map)
-    .bindPopup('Esta en la dirección de mi casa' + distancia)
-    //.openPopup();
-L.marker([-0.355708, -78.528793]).addTo(map)
-    .bindPopup('Esta es la dirección de la casa de Jazmín.')
-    .openPopup();
-L.marker([-0.225069,-78.5168959]).addTo(map)
-    .bindPopup('Aqui esta el instituto')
-    .openPopup();
-L.marker([-0.2537563,-78.5330725]).addTo(map)
-    .bindPopup('Esta es la dirección de la casa de Steven.')
-    .openPopup();
-L.marker([-0.0866811,-78.5073176]).addTo(map)
-    .bindPopup('Esta es la dirección de la casa de Brayan.')
-    .openPopup();
-L.marker([-0.2235145,-78.5113281]).addTo(map)
-    .bindPopup('Esta es la dirección de la casa de Jeniffer.')
-    .openPopup();
-
-L.marker([-0.071850, -78.435068]).addTo(map)
-    .bindPopup('Esta es la dirección de la casa de Francisco.')
-    .openPopup();
-
-L.marker([-0.290397, -78.552108]).addTo(map)
-    .bindPopup('Esta es la dirección de la casa de Kevin Quemag.')
-    .openPopup();
-
-L.marker([-0.241768, -78.526977]).addTo(map)
-    .bindPopup('Esta es la dirección de la casa de Michael.')
-    .openPopup();
-
-L.marker([-0.352055, -78.528046]).addTo(map)
-    .bindPopup('Esta es la dirección de la casa de Mauricio.')
-    .openPopup();
-
 var puntosCasaInsitutos = [
     [    [-0.257128, -78.546270],[-0.225069,-78.5168959]],
     [    [-0.355708, -78.528793],[-0.225069,-78.5168959]],
     [    [-0.2537563,-78.5330725],[-0.225069,-78.5168959]],
     [    [-0.0866811,-78.5073176],[-0.225069,-78.5168959]],
-    [    [-0.0866811,-78.5073176],[-0.225069,-78.5168959]],
+    [    [-0.2235145,-78.5113281],[-0.225069,-78.5168959]],
     [    [-0.071850, -78.435068],[-0.225069,-78.5168959]],
     [    [-0.290397, -78.552108],[-0.225069,-78.5168959]],
     [    [-0.241768, -78.526977],[-0.225069,-78.5168959]],
     [    [-0.352055, -78.528046],[-0.225069,-78.5168959]],
-
 ]
-for(let i=0 ;i<=puntosCasaInsitutos.length ;i++){
-    var polyline = L.polyline(puntosCasaInsitutos[i], {color: colorRandom()}).addTo(map);
+var compañerosCasa = ['Kevin Alexander(Yo)','Jazmin','Steven','Brayan','Jeniffer'
+                        ,'Francisco','Kevin Quemag','Michael','Mauricio']
+var distancias = [];
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+dibujarLineas(puntosCasaInsitutos);
+distanciaDosPuntos(puntosCasaInsitutos);
+dibujarPin(puntosCasaInsitutos,compañerosCasa);
+
+function dibujarPin(puntos,compañeros){
+    let auxArray = [];
+    let numeroCompañero=0;
+    for(let i =0; i<puntos.length;i++){
+        for(let j = 0; j<2;j++){
+            if(j==0)
+            {
+                auxArray = puntos[i][j];
+                L.marker(auxArray).addTo(map)
+                .bindPopup('Esta es la dirección de la casa de: '+compañeros[numeroCompañero]+ ' la distancia entre entre el Instituto y su casa es: '+ distancias[numeroCompañero])
+                .openPopup();
+                numeroCompañero++;
+            }
+
+        }
+    }
+    L.marker([-0.225069,-78.5168959]).addTo(map)
+    .bindPopup('Tecnológico De Turismo Y Patrimonio "Yavirac"')
+    .openPopup();
+} 
+
+function dibujarLineas (puntos){
+    for(let i=0 ;i<puntos.length ;i++){
+        var polyline = L.polyline(puntos[i], {color: colorRandom()}).addTo(map);
+    }
 }
+
 function colorRandom(){
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -63,55 +60,19 @@ function colorRandom(){
     return color;
 }
 
-//var distancia= puntosCasaInsitutos[0].distanceTo(puntosCasaInsitutos[1]);
-//console.log(distancia);
-
-/*
-var yoInstituto = [
-    [-0.257128, -78.546270],
-    [-0.225069,-78.5168959],
-];
-var jazInstituto = [
-    [-0.355708, -78.528793],
-    [-0.225069,-78.5168959],
-];
-var stevInstituto = [
-    [-0.2537563,-78.5330725],
-    [-0.225069,-78.5168959],
-];
-var bryInstituto = [
-    [-0.0866811,-78.5073176],
-    [-0.225069,-78.5168959],
-];
-var jennInstituto = [
-    [-0.0866811,-78.5073176],
-    [-0.225069,-78.5168959],
-];
-var francInstituto = [
-    [-0.071850, -78.435068],
-    [-0.225069,-78.5168959],
-];
-var quemagInstituto = [
-    [-0.290397, -78.552108],
-    [-0.225069,-78.5168959],
-];
-var michaelInstituto = [
-    [-0.241768, -78.526977],
-    [-0.225069,-78.5168959],
-];
-var mauricioInstituto = [
-    [-0.352055, -78.528046],
-    [-0.225069,-78.5168959],
-];
-*/
-/*
-var polyline = L.polyline(puntosCasaInsitutos[0], {color: 'red'}).addTo(map);
-var polyline = L.polyline(jazInstituto, {color: 'blue'}).addTo(map);
-var polyline = L.polyline(stevInstituto, {color: 'green'}).addTo(map);
-var polyline = L.polyline(bryInstituto, {color: 'yellow'}).addTo(map);
-var polyline = L.polyline(jennInstituto, {color: 'brown'}).addTo(map);
-var polyline = L.polyline(francInstituto, {color: 'aqua'}).addTo(map);
-var polyline = L.polyline(quemagInstituto, {color: 'pink'}).addTo(map);
-var polyline = L.polyline(michaelInstituto, {color: 'purple'}).addTo(map);
-var polyline = L.polyline(mauricioInstituto, {color: '#3388ff'}).addTo(map);*/
-
+function distanciaDosPuntos(puntos){
+    let distancia = 0;
+    let institutoX = -0.225069;
+    let institutoY = -78.5168959;
+    let auxArray = []
+    for(let i =0; i<puntos.length;i++){
+        for(let j = 0; j<2;j++){
+            if(j==0){  
+                auxArray = puntos[i][j];
+                distancia = Math.sqrt(Math.pow(institutoX-auxArray[0],2)+Math.pow(institutoY-auxArray[1],2))
+                distancias.push(distancia);
+                console.log(distancia);
+            }
+        }
+    }
+}
